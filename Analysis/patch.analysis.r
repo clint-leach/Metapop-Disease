cc283b7d5b5b4d21fb17e636741d9f8a29b3d7eb
clint-leach <clint.leach@gmail.com>
2015-03-26 14:51:17 -0600

    More trap plots

--- Analysis/patch.analysis.r
+++ Analysis/patch.analysis.r
@@ -111,13 +111,18 @@ lines(loess.smooth(patch$quality[patch$longevity == 40], patch$susc.col[patch$lo
 lines(loess.smooth(patch$quality[patch$longevity == 100], patch$susc.col[patch$longevity == 100]), lwd = 1.5, col = "black")
 
 #===============================================================================
-# Analyzing trap effect when both xi > 0
+# Analyzing trap effect
+
+load(paste(getwd(), "/Output/trap.RData", sep = ""))
 
 quality <- unique(out$quality)
 longevity <- unique(out$longevity)
 
+# Plotting probability of infection (i.e. time spent infected) as function of
+# the xi's, longevity, and quality
 pinf <- tapply(out$I, list(out$xi_im, out$xi_em, out$longevity, out$quality), median)
 
+# Plots for highest longevity
 par(mfrow = c(1, 2))
 plot(out$quality, out$I, type = "n", ylim = c(0, 0.4), main = "xi_em = 0")
 lines(supsmu(quality, pinf[1, 1, 10, ]), col = "black", pch = 20)
@@ -126,6 +131,85 @@ lines(supsmu(quality, pinf[2, 1, 10, ]), col = "red", pch = 20)
 plot(out$quality, out$I, type = "n", ylim = c(0, 0.4), main = "xi_em = 0.5")
 lines(supsmu(quality, pinf[1, 2, 10, ]), col = "black", pch = 20)
 lines(supsmu(quality, pinf[2, 2, 10, ]), col = "red", pch = 20)
+
+# Scatter plots for all longevities
+par(mfrow = c(2, 5))
+for(i in 1:length(longevity)){
+  plot(out$quality, out$I, type = "n", ylim = c(0, 0.4), main = longevity[i])
+  points(quality, pinf[1, 2, i, ], col = "black", pch = 20)
+  points(quality, pinf[2, 2, i, ], col = "red", pch = 20)
+}
+
+
+###
+
+# Boxplots for all longevities and xi_im = 0.5
+par(mfrow = c(2, 5))
+for(i in longevity){
+  sub <- subset(out, xi_em == 0.5 & xi_im == 0.5 & longevity == i)
+  boxplot(I ~ quality, data = sub)
+}
+
+# Boxplots for highest longevity and  both values of xi_im 
+par(mfrow = c(1, 2))
+boxplot(I ~ quality, data = out, subset = xi_em == 0.5 & xi_im == 0 & longevity > 3)
+boxplot(I ~ quality, data = out, subset = xi_em == 0.5 & xi_im == 0.5 & longevity > 3)
+
+###
+
+# Tufte-style boxplot for highest longevity and both xi_im side-by-side
+sub <- subset(out, xi_em == 0.5 & longevity > 3)
+
+p <- ggplot(sub, aes(x = as.factor(quality), y = I)) + theme_classic()
+p <- p + xlab("Quality") + ylab("Probability of infection") 
+p <- p + geom_tufteboxplot(aes(colour = factor(xi_im)), outlier.colour = "grey80", position = position_dodge(width = 1)) +
+  scale_y_continuous(expand = c(0, 0.01)) + scale_colour_manual(values = c("black", "red"))
+
+###
+
+# Multipanel figure comparing xi_im for three longevities
+longs <- longevity[c(4, 7, 10)]
+
+sub <- subset(out, xi_em == 0.5 & xi_im == 0 & longevity == longs[1])
+p1 <- ggplot(sub, aes(x = as.factor(quality), y = I)) + theme_classic()
+p1 <- p1 + xlab("Quality") + ylab("Probability of infection") 
+p1 <- p1 + geom_tufteboxplot(outlier.colour = "grey80", position = position_dodge(width = 1)) +
+  scale_y_continuous(expand = c(0, 0.01))
+
+sub <- subset(out, xi_em == 0.5 & xi_im == 0.5 & longevity == longs[1])
+p2 <- ggplot(sub, aes(x = as.factor(quality), y = I)) + theme_classic()
+p2 <- p2 + xlab("Quality") + ylab("Probability of infection") 
+p2 <- p2 + geom_tufteboxplot(outlier.colour = "grey80", position = position_dodge(width = 1)) +
+  scale_y_continuous(expand = c(0, 0.01))
+
+
+sub <- subset(out, xi_em == 0.5 & xi_im == 0 & longevity == longs[2])
+p3 <- ggplot(sub, aes(x = as.factor(quality), y = I)) + theme_classic()
+p3 <- p3 + xlab("Quality") + ylab("Probability of infection") 
+p3 <- p3 + geom_tufteboxplot(outlier.colour = "grey80", position = position_dodge(width = 1)) +
+  scale_y_continuous(expand = c(0, 0.01))
+
+sub <- subset(out, xi_em == 0.5 & xi_im == 0.5 & longevity == longs[2])
+p4 <- ggplot(sub, aes(x = as.factor(quality), y = I)) + theme_classic()
+p4 <- p4 + xlab("Quality") + ylab("Probability of infection") 
+p4 <- p4 + geom_tufteboxplot(outlier.colour = "grey80", position = position_dodge(width = 1)) +
+  scale_y_continuous(expand = c(0, 0.01))
+
+
+sub <- subset(out, xi_em == 0.5 & xi_im == 0 & longevity == longs[3])
+p5 <- ggplot(sub, aes(x = as.factor(quality), y = I)) + theme_classic()
+p5 <- p5 + xlab("Quality") + ylab("Probability of infection") 
+p5 <- p5 + geom_tufteboxplot(outlier.colour = "grey80", position = position_dodge(width = 1)) +
+  scale_y_continuous(expand = c(0, 0.01))
+
+sub <- subset(out, xi_em == 0.5 & xi_im == 0.5 & longevity == longs[3])
+p6 <- ggplot(sub, aes(x = as.factor(quality), y = I)) + theme_classic()
+p6 <- p6 + xlab("Quality") + ylab("Probability of infection") 
+p6 <- p6 + geom_tufteboxplot(outlier.colour = "grey80", position = position_dodge(width = 1)) +
+  scale_y_continuous(expand = c(0, 0.01))
+
+grid.arrange(p1, p3, p5, p2, p4, p6, nrow = 2)
+
 
 #===============================================================================
 # Consequences of trap effect (with xi_em = 0.5)
@@ -163,4 +247,13 @@ lines(log10(longevity), pan[, 2], type = "b", col = "red", pch = 20)
 plot(log10(longevity), ext[,1], type = "b", pch = 20, ylim = c(0, 1), main = "extinction")
 lines(log10(longevity), ext[, 2], type = "b", col = "red", pch = 20)
 
+
+###
+
+# Stand-along plot of extinction probability
+
+par(mfrow = c(1, 1))
+plot(log10(longevity[c(4, 7, 10)]), ext[c(4, 7, 10), 1], type = "b", pch = 20, ylim = c(0, 0.4),
+     xlab = "log(longevity)", ylab = "Probability of extinction", bty = "l")
+lines(log10(longevity[c(4, 7, 10)]), ext[c(4, 7, 10), 2], type = "b", pch = 20, col = "red")
 
